162e6c561fc9b65ba99f218cc0132b3eec020da4
ravi0li <29655971+r4v10l1@users.noreply.github.com>
2021-11-19 11:48:50 +0000

    006

---
+++ 006/fahrenheit2celsius-function.c
@@ -0,0 +1,15 @@
+// 1.15
+
+#include <stdio.h>
+
+float fahr2cel(int celsius);
+
+main() {
+	for (int n = 0; n <= 300; n = n+20){
+		printf("Testing %3d: %6.2f\n", n, fahr2cel(n));
+	}
+}
+
+float fahr2cel(int f) {
+	return (5.0/9.0)*(f-32);
+}
