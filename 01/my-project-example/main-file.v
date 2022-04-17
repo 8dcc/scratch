6afac3a1783f41165d0b249e19d7ccdded01ba4b
r4v10l1 <29655971+r4v10l1@users.noreply.github.com>
2022-04-17 14:45:16 +0200

    Example of project with several files

---
+++ 01/my-project-example/main-file.v
@@ -0,0 +1,13 @@
+/*
+This folder is an example of the `v run .` functionality
+This will be the entry point of our program.
+
+Wanna learn V with a lamar vibe? 
+Check https://www.youtube.com/channel/UCpaTqf90rm1jmJI49BtVMRw
+*/
+
+fn main() {
+	println("Entry point of our program! Calling other functions...")
+	custom_helper()
+	println(mult_function(5))
+}
