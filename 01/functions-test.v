72fa2de4b1c127dc6c8b041cbdc2482ef438c7e4
r4v10l1 <29655971+r4v10l1@users.noreply.github.com>
2022-04-18 20:02:48 +0200

    Updated functions-test.v

--- 01/functions-test.v
+++ 01/functions-test.v
@@ -3,10 +3,11 @@ Basically what I did with the project folder thing
 */
 
 fn main() {
-	print("Calling function add: ")
-	println(add(5, 2))
-	print("Calling function sub: ")
-	println(sub(5, 2))
+	println("Calling function add: ${add(1, 2)}")
+	println("Calling function sub: ${sub(5, 2)}")
+	println("Calling function multple_returns: ${multiple_returns('foo', 'bar')}")
+	mtest1, mtest2 := multiple_returns('foo', 'bar')
+	println("Printing variables mtest1 and mtest2: $mtest1 $mtest2")
 }
 
 fn add(val1 int, val2 int) int {
@@ -15,4 +16,8 @@ fn add(val1 int, val2 int) int {
 
 fn sub(val1 int, val2 int) int {
 	return val1 - val2
+}
+
+fn multiple_returns(val1 string, val2 string) (string, string) {
+	return val2, val1
 }
