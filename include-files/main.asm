932c79b1086ae2e0d64a55205fb581d1bda7fd99
r4v10l1 <29655971+r4v10l1@users.noreply.github.com>
2022-06-09 17:45:08 +0200

    Add main.asm to include-files folder

---
+++ include-files/main.asm
@@ -0,0 +1,17 @@
+%include 'my-include.asm'
+
+section .data
+    str1    db      "I am a test string!",0xA
+    str2    db      "I am also a different test string!",0xA
+
+section .text
+    global _start
+
+_start:
+    mov     eax,str1        ; Move str1 to eax (as func argument)
+    call    sprint          ; Call sprint function from my-include.asm
+
+    mov     eax,str2
+    call    sprint          ; Print str2
+
+    call    quit            ; Call quit function from my-include.asm
