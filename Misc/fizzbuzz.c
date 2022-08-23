5d2f249a64d51e6628e4854fbe513ea88b2c4e50
r4v10l1 <29655971+r4v10l1@users.noreply.github.com>
2022-08-23 19:55:46 +0200

    Add fizzbuzz program

---
+++ Misc/fizzbuzz.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+void fizzbuzz_loop(const int iterations);
+const char* int2fizzbuzz(const int n);
+
+int main(int argc, char** argv) {
+    if (argc < 2) {
+        printf("I need the number of rounds!\n");
+        return 1;
+    }
+
+    fizzbuzz_loop(atoi(argv[1]));       // Pass arg as int to func
+    return 0;
+}
+
+void fizzbuzz_loop(const int iterations) {
+    for (int n = 0; n < iterations; n++) {
+        if (n % 15 == 0)        printf("FizzBuzz");
+        else if (n % 3 == 0)    printf("Fizz");
+        else if (n % 5 == 0)    printf("Buzz");
+        else                    printf("%d", n);
+
+        printf("\n");
+    }
+}
+
