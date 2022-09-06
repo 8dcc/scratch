3e0e026593777f7db0714222ac2b55bdf141af15
r4v10l1 <29655971+r4v10l1@users.noreply.github.com>
2022-09-06 18:50:52 +0200

    Move fahrenheit exercises to 01

--- Book/01/1-3-fahrenheit2celsius.c
+++ Book/01/1-3-fahrenheit2celsius.c
@@ -1,7 +1,7 @@
+/* 1-3 */
+
 #include <stdio.h>
 
-/* Comment ok shut up */
-// Another comment shut the fuck up
 main(){
 	int fahr, celsius;
 	int lower, upper, step;
@@ -10,11 +10,14 @@ main(){
 	upper = 300;	// Higher limit of temp scale
 	step = 10;		// Step size
 
+    printf("Fahr    | Celsius\n");
+    printf("========|========\n");
+
 	fahr = lower;
 	while (fahr <= upper) {
 		// We don't multiply *5/9 because c truncates so we would multiply *0
 		celsius = 5 * (fahr-32) / 9;
-		printf("%d\t%d\n", fahr, celsius);	// Replace %d with int variable
+		printf("%d\t| %d\n", fahr, celsius);	// Replace %d with int variable
 		fahr = fahr + step;
 	}
 }
--- Book/01/1-4-celsius2fahrenheit.c
+++ Book/01/1-4-celsius2fahrenheit.c
@@ -1,7 +1,7 @@
+/* 1-4 */
+
 #include <stdio.h>
 
-/* Comment ok shut up */
-// Another comment shut the fuck up
 main(){
 	int fahr, celsius;
 	int lower, upper, step;
@@ -10,14 +10,14 @@ main(){
 	upper = 300;	// Higher limit of temp scale
 	step = 10;		// Step size
 
-	fahr = lower;
-	while (fahr <= upper) {
-		// We don't multiply *5/9 because c truncates so we would multiply *0
-		celsius = 5 * (fahr-32) / 9;
+	celsius = lower;
+	while (celsius <= upper) {
+		// We don't multiply *9/5 because c truncates so we would multiply *1
+		fahr = (celsius * 9) / 5 + 32;
 
 		// Make a '3 digit holder' and a '6 digit holder' so it's right-justified
-		printf("%3d %6d\n", fahr, celsius);
+		printf("%3d %6d\n", celsius, fahr);
 
-		fahr = fahr + step;
+		celsius = celsius + step;
 	}
 }
