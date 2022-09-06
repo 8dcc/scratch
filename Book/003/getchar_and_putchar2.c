4116b533208f81b4f24f275f3ab68155d4151244
r4v10l1 <29655971+r4v10l1@users.noreply.github.com>
2022-09-06 18:58:48 +0200

    Remove pointless files

--- Book/003/getchar_and_putchar2.c
+++
@@ -1,9 +0,0 @@
-#include <stdio.h>
-
-main() {
-	int c;  // Instead of char we read int to be able to understand EOF
-
-	while ((c = getchar()) != EOF) {	// Will read and display (add to queue)
-		putchar(c);						// the character until EOF
-	}
-}
