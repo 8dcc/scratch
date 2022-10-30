5c34ae5da225a3012bae3174c92cc65017e5ca90
8dcc <8dcc.git@gmail.com>
2022-10-30 22:06:27 +0100

    Add original hello world and folder for 02

--- 02-exit/exit.asm
+++ 02-exit/exit.asm
@@ -19,3 +19,7 @@ _start:
     mov ecx,helloMsg                        ; helloMsg pointer as 2nd argument
     mov edx,helloLen                        ; String length from the .data section
     int 0x80                                ; Interrupt with code 0x80 (call kernel)
+
+    mov eax,1                               ; sys_exit syscall (1)
+    mov ebx,0                               ; Exit code (return 0)
+    int 0x80                                ; Call kernel again
