8890c40db82d63d5e217a6a76d14a0e1fa609669
8dcc <8dcc.git@gmail.com>
2022-11-07 13:10:44 +0100

    Add misc/05-count-args

---
+++ misc/05-count-args/count-args.asm
@@ -0,0 +1,34 @@
+; Simple program that iterates and prints the provided arguments
+; Arg number only works if they are less than 10 args
+
+%include 'simple-lib.asm'
+
+section .data
+    tab_ptr db 0x9         ; '\t'
+
+section .text
+    global _start
+
+_start:
+    pop     edx             ; Save the value on top of the stack in ecx.
+                            ; This is the number of arguments (argc).
+
+    mov     ecx, 0          ; Same as "xor ecx, ecx"
+
+.loop:
+    cmp     ecx, edx        ; First, check if we printed all args
+    jge      .done          ; If there are no args left, break (edx >= ecx)
+
+    ; Print ecx number + '\t' before arg itself
+    mov     eax, ecx        ; Move the current iteration to eax
+    call    iprint          ; Call printn(int) to print the char
+    mov     eax, tab_ptr    ; Print tab char
+    call    sprint
+
+    pop     eax             ; Save argv[ecx] into eax
+    call    println         ; Print eax (Keep in mind argv is a char**)
+    inc     ecx             ; Increase current arg count
+    jmp     .loop
+
+.done:
+    call    quit            ; We done
