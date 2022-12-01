45653a73a1c2bd029ae2ea397c26b266c6e706ed
8dcc <8dcc.git@gmail.com>
2022-12-01 23:29:40 +0100

    Add 4 byte paddings for args

--- asmtutor/30-bind-socket/bind-socket.asm
+++ asmtutor/30-bind-socket/bind-socket.asm
@@ -22,6 +22,9 @@ _start:
 
 .bind:
     mov     edi, eax        ; Save FD we got when creating the socket in edi
+    push    dword 0x0       ; (4 BYTE PADDING) Figured out why the arg size is 16. See:
+                            ; https://www.scip.ch/en/?labs.20200521 (Bind the socket part)
+    push    dword 0x0       ; (4 BYTE PADDING)
     push    dword 0x0       ; (IP ADDRESS) Push the double word (32bit = 4bytes) 0 into the stack (0.0.0.0)
     push    word 0x2923     ; (PORT) 9001 dec = 0x2329 hex. Because we are pushing we need to reverse the bytes.
                             ; Important note: In x86 asm, a dword is 32bits and a word is 16bits! So reversing
@@ -33,10 +36,12 @@ _start:
     push    edi             ; Push the FD from the socket
     mov     ecx, esp        ; Current stack layout:
                             ; ...               | Bottom of the stack (Bigger address in memory because stack goes down)
+                            ; 0x00000000        | Padding 1
+                            ; 0x00000000        | Padding 2
                             ; 0x00000000        | IP ADDRESS
                             ; 0x2923            | PORT
                             ; 0X0002      <--   | AF_INET
-                            ; 0x10 (16)     |   | Size of arguments we just pushed. TODO: Don't really get why it's 16
+                            ; 0x10 (16)     |   | Size of arguments we just pushed. (4 + 4 + 4 + 2 + 2)
                             ; ptr -----------   | Pointer to args
                             ; FD                | File descriptor we got from last sys_socketcall. We move this address to ecx
                             ; ...               | Top of the stack (Lower address in memory)
