87d2fced4fe67ac037dd8891a51c26f765a45b31
8dcc <8dcc.git@gmail.com>
2023-04-01 15:31:24 +0200

    Use malloc instead of .bss, add str++

--- codewars/backspaces.asm
+++ codewars/backspaces.asm
@@ -1,39 +1,45 @@
 ; Kata: https://www.codewars.com/kata/5727bb0fe81185ae62000ae3
-; FIXME
-
-section .bss
-  buf resb 1000
 
 section .text
-global strclr
+    global strclr
+    extern malloc
 
 ; [byte RAX] strclr(ro [byte RDI] s)
 strclr:
+    push    rdi                 ; rax = malloc(1000)
+    xor     rax, rax
+    mov     rdi, 1000
+    call    malloc
+    pop     rdi
+
+    mov     rcx, rax            ; rcx = original allocated buf
+    mov     rsi, rcx            ; rsi used to move the current buf ptr
+    xor     rax, rax
     xor     rdx, rdx
-    mov     rsi, buf            ; rsi used for the buf location
 
 .loop:
     mov     dl, byte [rdi]      ; dl = *str;
     test    dl, dl              ; if (*str == '\0')
     jz      .done               ;   break;
 
+    inc     rdi                 ; str++;
+
     cmp     dl, 35              ; if (*str == '#')
     jne     .not_backspace
 
-    cmp     rsi, buf            ;   if (buf > original_buf) 
+    cmp     rsi, rcx            ;   if (buf > original_buf)
     jle     .loop
 
     dec     rsi                 ;     buf--;
     jmp     .loop               ;     continue;
 
 .not_backspace:
-    mov     [rsi], dl           ; *buf = *str;
+    mov     byte [rsi], dl      ; *buf = *str;
     inc     rsi                 ; buf++;
-
     jmp     .loop               ; continue;
 
 .done:
     mov     [rsi], byte 0       ; *buf = '\0';
-    mov     rax, buf            ; return original_buf;
+    mov     rax, rcx            ; return original_buf;
     ret
 
