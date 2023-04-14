460107b4fe39d441fb794a912c93654a9fcd995f
8dcc <8dcc.git@gmail.com>
2023-04-15 00:56:02 +0200

    Add Chapter 2.14: Functions as Objects

--- src/ansi-common-lisp.lisp
+++ src/ansi-common-lisp.lisp
@@ -4,7 +4,7 @@
         (var2 13)
         (var3 10))
     (+ var1 var2 var3))
-  (let ((var1 7))           ; Note the list of declarations even with only 1
+  (let ((var1 7))       ; Note the list of declarations even with only 1
     (- var1 5)))
 
 (test-let)
@@ -24,3 +24,32 @@
 
 (test-do 5)
 ;; Chapter 2.13: Iteration:1 ends here
+
+;; [[file:ansi-common-lisp.org::*Chapter 2.14: Functions as Objects][Chapter 2.14: Functions as Objects:1]]
+(lambda (x y)       ; No name
+  (* x y))
+;; Chapter 2.14: Functions as Objects:1 ends here
+
+;; [[file:ansi-common-lisp.org::*Chapter 2.14: Functions as Objects][Chapter 2.14: Functions as Objects:2]]
+((lambda (x) (* x x)) 5)    ; 5 is a parameter
+;; Chapter 2.14: Functions as Objects:2 ends here
+
+;; [[file:ansi-common-lisp.org::*Chapter 2.14: Functions as Objects][Chapter 2.14: Functions as Objects:3]]
+(let ((mylambda #'(lambda (x y)
+                  (format t "I am an expression inside the lambda!~%")
+                  (* x y))))
+  (format t "Begining of the let expression block!~%")
+  (format t "Lambda return: ~A~%" (funcall mylambda 5 3)))
+;; Chapter 2.14: Functions as Objects:3 ends here
+
+;; [[file:ansi-common-lisp.org::*Chapter 2.14: Functions as Objects][Chapter 2.14: Functions as Objects:4]]
+;;  'symbol -> (quote symbol)
+;; #'symbol -> (function symbol)
+(funcall #'(lambda (x)
+             (format t "(funcall) Value of x: ~A~%" x)) ; Body of lambda
+         "Test 1")                                      ; Argument
+
+(apply #'(lambda (x)
+           (format t "  (apply) Value of x: ~A~%" x))   ; Body of lambda
+       '("Test 2"))                                     ; Argument. Needs list
+;; Chapter 2.14: Functions as Objects:4 ends here
