b0a59569340961b92f22d63b5179864e8d136c76
8dcc <8dcc.git@gmail.com>
2023-04-16 01:19:33 +0200

    Add exercises

--- src/ansi-common-lisp.lisp
+++ src/ansi-common-lisp.lisp
@@ -53,3 +53,59 @@
            (format t "  (apply) Value of x: ~A~%" x))   ; Body of lambda
        '("Test 2"))                                     ; Argument. Needs list
 ;; Chapter 2.14: Functions as Objects:4 ends here
+
+;; [[file:ansi-common-lisp.org::*Exercises][Exercises:1]]
+(+ (- 5 1) (+ 3 7))
+;; Exercises:1 ends here
+
+;; [[file:ansi-common-lisp.org::*Exercises][Exercises:2]]
+(list 1 (+ 2 3))
+;; Exercises:2 ends here
+
+;; [[file:ansi-common-lisp.org::*Exercises][Exercises:3]]
+(if (listp 1)
+    (+ 1 2)
+    (+ 3 4))
+;; Exercises:3 ends here
+
+;; [[file:ansi-common-lisp.org::*Exercises][Exercises:4]]
+(list (and (listp 3) t) (+ 1 2))
+;; Exercises:4 ends here
+
+;; [[file:ansi-common-lisp.org::*Exercises][Exercises:5]]
+(cons 'a (cons 'b (cons 'c nil)))
+;; Exercises:5 ends here
+
+;; [[file:ansi-common-lisp.org::*Exercises][Exercises:6]]
+(cons 'a '(b c))
+;; Exercises:6 ends here
+
+;; [[file:ansi-common-lisp.org::*Exercises][Exercises:7]]
+(cons 'a (cons 'b '(c)))
+;; Exercises:7 ends here
+
+;; [[file:ansi-common-lisp.org::*Exercises][Exercises:8]]
+(defun fourth-elem (lst)
+  (car (cdr (cdr (cdr lst)))))
+
+(fourth-elem '(10 20 30 40 50))
+;; Exercises:8 ends here
+
+;; [[file:ansi-common-lisp.org::*Exercises][Exercises:9]]
+(defun greater (x y)
+  (if (> x y)
+      x
+      y))
+
+(greater 5 12)
+;; Exercises:9 ends here
+
+;; [[file:ansi-common-lisp.org::*Exercises][Exercises:10]]
+(defun enigma (x)
+  (and (not (null x))
+       (or (null (car x))
+           (enigma (cdr x)))))
+
+(enigma '(1 2 3))
+(enigma '(1 2 nil 4))
+;; Exercises:10 ends here
