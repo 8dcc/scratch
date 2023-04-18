7fe84722629a6fb51ec465266c65ebcc2a5cd608
8dcc <8dcc.git@gmail.com>
2023-04-18 13:56:16 +0200

    Add chapter 3.5: Compression
    Compress and decompress functions, tests and explanations.

--- src/ansi-common-lisp.lisp
+++ src/ansi-common-lisp.lisp
@@ -242,3 +242,59 @@
       (third x) 0)
 (format t "x: ~A, y: ~A, eql: ~A~%" x y (eql x y))
 ;; Chapter 3.3: Why lisp has no pointers:3 ends here
+
+;; [[file:ansi-common-lisp.org::*Compress][Compress:1]]
+(defun compress (x)                                             ; (7)
+  (if (consp x)
+      (compress-list (car x) 1 (cdr x))
+      x))
+
+(defun compress-list (last-item last-n rest)                    ; (2)
+  (if (null rest)
+      (list (n-items last-item last-n))                         ; (3)
+      (let ((next-item (car rest)))
+        (if (= last-item next-item)                             ; (4)
+            (compress-list last-item (+ last-n 1) (cdr rest))   ; (5)
+            (cons (n-items last-item last-n)                    ; (6)
+                  (compress-list next-item 1 (cdr rest)))))))
+
+
+(defun n-items (item n)                                         ; (1)
+  (if (> n 1)
+      (list n item)
+      item))
+
+(compress '(1 0 0 1 1 0 1 1 1 1 0 0 1 0 1 1 0))
+;; Compress:1 ends here
+
+;; [[file:ansi-common-lisp.org::*Decompress][Decompress:1]]
+(defun decompress (lst)
+  (if (null lst)                                    ; (2)
+      nil
+      (let ((cur-item (car lst))                    ; (3)
+            (rest (decompress (cdr lst))))
+        (if (consp cur-item)                        ; (4)
+            (append (apply #'expand-items cur-item) ; (5)
+                    rest)
+            (cons cur-item rest)))))                ; (6)
+
+(defun expand-items (n item)                        ; (1)
+  (if (zerop n)
+      nil
+      (cons item (expand-items (- n 1) item))))
+
+(decompress '(10 20 (4 7) 40 (3 5) 60))
+(decompress '(1 (2 0) (2 1) 0 (4 1) (2 0) 1 0 (2 1) 0))
+;; Decompress:1 ends here
+
+;; [[file:ansi-common-lisp.org::*Decompress][Decompress:2]]
+    (append (expand-items (car rest)
+                        (second rest))
+            rest)
+;; Decompress:2 ends here
+
+;; [[file:ansi-common-lisp.org::*Both combined][Both combined:1]]
+(let ((test '(1 0 0 1 1 0 1 1 1 1 0 0 1 0 1 1 0)))
+  (equal test
+         (decompress (compress test))))
+;; Both combined:1 ends here
