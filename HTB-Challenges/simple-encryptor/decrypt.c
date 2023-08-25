7bd64c19f05ba058dda0b0b223698e1e1634e1d8
8dcc <8dcc.git@gmail.com>
2023-08-25 18:42:03 +0200

    Fix seed reading, don't print EOF

--- HTB-Challenges/simple-encryptor/decrypt.c
+++ HTB-Challenges/simple-encryptor/decrypt.c
@@ -15,52 +15,32 @@ int main() {
     *ptr = EOF;
 
     /* main+0x15C -> main+0x170 */
-    uint32_t seed = 0;
-    for (int i = 0; i < 4; i++) {
-        seed <<= 8;
-        seed += buf[i];
-    }
-
-    printf("Seed: %d\n", seed);
-
+    uint32_t seed = *(uint32_t*)buf;
     srand(seed);
 
+    fprintf(stderr, "Seed: %d\n", seed);
+
 #ifdef TEST_ROTATIONS
+    srand(1337);
+
     char test0 = 'a';
     printf("%c -> ", test0);
 
     /* ROL(test0, r); */
-    int r = rand() & 7;
+    uint32_t r    = rand() & 7;
     uint8_t test1 = (test0 << r) | (test0 >> (8 - r));
     printf("%d -> ", test1);
 
     /* ROR(test1, r); */
     test0 = (test1 >> r) | (test1 << (8 - r));
     printf("%c\n", test0);
+
+    srand(seed);
 #endif
 
-#ifdef ENCRYPT_CODE
-    for (int i = 4; buf[i] != EOF; i++) {
-        /* main+0xC7 -> main+0xCC */
-        uint32_t r = rand();
-
-        /* main+0xCF -> main+0xF2 */
-        buf[i] ^= r;
-
-        /* main+0xF4 -> main+0xFC */
-        r = rand() & 7;
-
-        /* main+0xFE -> main+0x10C */
-        uint8_t tmp = buf[i];
-
-        /* main+0x10F -> main+0x130 (optimized) */
-        buf[i] = (tmp << r) | (tmp >> (8 - r));
-    }
-#endif
-
-    /* Reverse code of the loop above */
-    /* TODO: Should be fine but it doesn't quite work */
-    for (int i = 4; buf[i] != EOF; i++) {
+    /* Opposite code of encrypt.c */
+    int i;
+    for (i = 4; buf[i] != EOF; i++) {
         uint32_t r1 = rand();
         uint32_t r2 = rand() & 7;
 
@@ -71,8 +51,8 @@ int main() {
         buf[i] ^= r1;
     }
 
+    buf[i] = '\0';
     printf("%s\n", &buf[4]);
 
     return 0;
 }
-
