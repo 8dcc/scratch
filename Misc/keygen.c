66e7e69c1aae3fde9fcf2638e5abc23ae9f241ac
8dcc <8dcc.git@gmail.com>
2024-01-05 22:05:51 +0100

    Hopefully improve the keygen algorithm

--- Misc/keygen.c
+++ Misc/keygen.c
@@ -2,43 +2,49 @@
 #include <stdbool.h>
 #include <stdint.h>
 #include <stdio.h>
+#include <string.h>
 #include <stdlib.h>
 
 #define KEY_LEN 20
 
 static void generate_key(const char* user, uint8_t* out) {
-    int user_pos   = 0;
-    bool user_done = false;
+    const int user_len = strlen(user);
+    int user_pos       = 0;
 
     for (int i = 0; i < KEY_LEN; i++) {
         unsigned char c = user[user_pos];
 
         /* Swap every bit pair with the adjactent pair */
+        c = ((c & 0x33333333) << 2) | ((c & 0xCCCCCCCC) >> 2);
+
+        /* Swap every bit with the adjactent one */
         c = ((c & 0x55555555) << 1) | ((c & 0xAAAAAAAA) >> 1);
 
         /* XOR the first character by 0x33, and the rest by the previous one */
         c ^= (i == 0) ? 0x33 : out[i - 1];
-
-        /* Depending on i, change the output a bit */
-        if (i % 2 == 0)
-            c ^= 1;
 
         /* Write the char to the current position */
         out[i] = c;
 
         /* If there are no chars left in the input, go to the begining again */
         user_pos++;
-        if (user[user_pos] == '\0') {
-            user_done = true;
-            user_pos  = 0;
-        }
+        if (user[user_pos] == '\0')
+            user_pos = 0;
     }
 
-    /* We still have characters left in the user */
-    if (!user_done) {
-        int key_pos = 0;
+    /* XOR the output with the key N-1 times more */
+    int key_pos = 0;
+    for (int i = 0; i < user_len - 1; i++) {
+        user_pos = 0;
         while (user[user_pos] != '\0') {
-            out[key_pos++] ^= user[user_pos++];
+            out[key_pos] ^= user[user_pos];
+
+            /* Depending on i and the length of the input, change the output */
+            if (user_pos % 2 == 0)
+                out[key_pos] ^= user_len & 0xFF;
+
+            user_pos++;
+            key_pos++;
 
             /* Don't overflow the key if the user is bigger than KEY_LEN*2 */
             if (key_pos >= KEY_LEN)
