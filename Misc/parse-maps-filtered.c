3d9504fde5eac517060fba98cf7471f26a2eb56a
8dcc <8dcc.git@gmail.com>
2024-03-30 16:25:42 +0100

    Finish parse-maps-filtered.c
    For now it has to literally match the module name in maps (absolute
    path)

--- Misc/parse-maps-filtered.c
+++ Misc/parse-maps-filtered.c
@@ -1,10 +1,19 @@
-/* Parsing /proc/self/maps to get the current module location and size in
- * memory. */
+/*
+ * Parse /proc/self/maps to get the start and end addresses of the specified
+ * module.
+ *
+ * The function assumes the format of maps is always:
+ * 0000DEADBEEF-0000ABADCAFE rwxp 000123AB 103:03 12345678   /path/module
+ *
+ * NOTE: You can replace most calls to isspace() and remove the <ctype.h>
+ * include by just checking if c is a space.
+ */
 
 #include <stdint.h>
 #include <stdbool.h>
 #include <stdio.h>
-#include <stdlib.h>
+#include <stdlib.h> /* strtoull() */
+#include <ctype.h>  /* isspace() */
 
 static void get_module_bounds(const char* module_name, uintptr_t* start,
                               uintptr_t* end) {
@@ -17,6 +26,7 @@ static void get_module_bounds(const char* module_name, uintptr_t* start,
     if (!fd)
         return;
 
+    /* For converting to uint64_t using strtoull() */
     static char addr_buf[] = "FFFFFFFFFFFFFFFF";
     int addr_buf_pos;
 
@@ -42,42 +52,52 @@ static void get_module_bounds(const char* module_name, uintptr_t* start,
         /* Parse "rwxp". For now we only care about read permissions. */
         bool is_readable = ((c = fgetc(fd)) == 'r');
 
-        if (module_name != NULL) {
-            /* Skip permissions */
-            while ((c = fgetc(fd)) != ' ')
+        bool name_matches = true;
+        if (module_name == NULL) {
+            /* We don't want to filter the module name, skip until end of line
+             * or end of file. */
+            while ((c = fgetc(fd)) != '\n' && c != EOF)
+                ;
+        } else {
+            /* Skip permissions and single space */
+            while (!isspace(c = fgetc(fd)))
                 ;
 
-            /* Skip space */
-            c = fgetc(fd);
-
-            /* Skip 3rd column */
-            while ((c = fgetc(fd)) != ' ')
+            /* Skip 3rd column and single space */
+            while (!isspace(c = fgetc(fd)))
                 ;
 
-            /* Skip space */
-            c = fgetc(fd);
-
-            /* Skip 4th column */
-            while ((c = fgetc(fd)) != ' ')
+            /* Skip 4th column and single space */
+            while (!isspace(c = fgetc(fd)))
                 ;
-
-            /* Skip space */
-            c = fgetc(fd);
 
             /* Skip 5th column */
-            while ((c = fgetc(fd)) != ' ')
+            while (!isspace(c = fgetc(fd)))
                 ;
 
-            /* Skip separator until the module name */
-            while ((c = fgetc(fd)) == ' ')
+            /* Skip spacing until the module name. First char of module name
+             * will be saved in `c' after this loop. */
+            while (isspace(c = fgetc(fd)))
                 ;
 
-            /* TODO: Compare module name. Except the output of maps has absolute
-             * paths */
+            /* Compare module name. Note that the output of maps has absolute
+             * paths. */
+            int i = 0;
+            do {
+                /* A character did not match the name of the module in this
+                 * line, ignore it. We can't break out of the `for' because we
+                 * have to get to the newline anyway. */
+
+                if (c != module_name[i])
+                    name_matches = false;
+                else if (module_name[i] != '\0')
+                    i++;
+            } while ((c = fgetc(fd)) != '\n');
         }
 
-        /* We can read it, otherwise ignore */
-        if (is_readable) {
+        /* We can read it, and it's the module we are looking for, check if the
+         * addresses are smaller/bigger. Otherwise ignore. */
+        if (is_readable && name_matches) {
             if (*start > start_addr || !start_set) {
                 *start    = start_addr;
                 start_set = true;
@@ -88,10 +108,6 @@ static void get_module_bounds(const char* module_name, uintptr_t* start,
                 end_set = true;
             }
         }
-
-        /* Skip until end of line or end of file */
-        while ((c = fgetc(fd)) != '\n' && c != EOF)
-            ;
     }
 
     fclose(fd);
@@ -106,8 +122,11 @@ static void print_maps(void) {
 }
 
 int main(void) {
+    /* Use NULL to disable name filtering */
+    static const char* MODULE_NAME = "/usr/lib/libc.so.6";
+
     uintptr_t start, end;
-    get_module_bounds("parse-maps-filtered.out", &start, &end);
+    get_module_bounds(MODULE_NAME, &start, &end);
 
     printf("File output:\n");
     print_maps();
