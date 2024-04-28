35f4252350125d9585ebb85a3c07c03dec5313a6
8dcc <8dcc.git@gmail.com>
2024-04-28 15:06:56 +0200

    Add asm-callstack

---
+++ Misc/asm-callstack/callstack.asm
@@ -0,0 +1,48 @@
+
+default rel
+extern printf
+
+section .data
+    fmt: db "%d: %p", 0xA, 0x0
+
+section .text
+
+; void AsmPrintCallStack(int depth);
+global AsmPrintCallStack
+AsmPrintCallStack:
+    push    rbp
+    mov     rbp, rsp
+    push    r12
+    push    r13
+
+    ; Initialize counter to the first "depth" argument
+    mov     r13, rdi
+
+    ; Get address of old RBP, skip ours
+    mov     r12, [rbp]
+
+.loop:
+    ; If RBP is NULL, we are done.
+    ; We add 8 to the current RBP to get the current return address.
+    test    r13, r13
+    jz      .done
+
+    ; Otherwise, we can print it.
+    ;   printf("%d: %p\n", i, cur_ret);
+    mov     rdx, [r12 + 0x8]
+    mov     rsi, r13
+    mov     rdi, fmt
+    call    printf wrt ..plt
+
+    ; And jump to the next one
+    ;   i--; rbp = *rbp; continue;
+    dec     r13
+    mov     r12, [r12]
+    jmp     .loop
+
+.done:
+    pop     r13
+    pop     r12
+    mov     rsp, rbp
+    pop     rbp
+    ret
---
+++ Misc/asm-callstack/main.c
@@ -0,0 +1,39 @@
+#include <stdio.h>
+
+extern void AsmPrintCallStack(int depth);
+
+void DrawPoint(int x, int y) {
+    printf("Drawing point: %d, %d\n", x, y);
+
+    AsmPrintCallStack(3);
+}
+
+void DrawLine(int x0, int y0, int x1, int y1) {
+    for (int y = y0; y <= y1; y++)
+        for (int x = x0; x <= x1; x++)
+            DrawPoint(x, y);
+}
+
+void DrawSquare(int x, int y, int w, int h) {
+    int x1 = x + w;
+    int y1 = y + h;
+
+    DrawLine(x, y, x1, y);   /* Up */
+    DrawLine(x, y, x, y1);   /* Left */
+    DrawLine(x, y1, x1, y1); /* Down */
+    DrawLine(x1, y, x1, y1); /* Right */
+}
+
+/*----------------------------------------------------------------------------*/
+
+int main(void) {
+    printf("(main) Address of DrawPoint:  %p\n", DrawPoint);
+    printf("(main) Address of DrawLine:   %p\n", DrawLine);
+    printf("(main) Address of DrawSquare: %p\n", DrawSquare);
+    printf("(main) Address of main:       %p\n", main);
+
+    /* DrawPoint only gets called once */
+    DrawSquare(5, 5, 0, 0);
+
+    return 0;
+}
