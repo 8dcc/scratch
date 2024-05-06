4161c4a5aeff96bb7d63128d4fe5af8ce1590c42
8dcc <8dcc.git@gmail.com>
2024-05-06 23:45:44 +0200

    Add LaTeX/clean.sh

---
+++ LaTeX/clean.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+read -p "Press any key to clean all folders..."
+
+rm -fv ./*/*.4ct
+rm -fv ./*/*.4tc
+rm -fv ./*/*.aux
+rm -fv ./*/*.css
+rm -fv ./*/*.dvi
+rm -fv ./*/*.html
+rm -fv ./*/*.idv
+rm -fv ./*/*.lg
+rm -fv ./*/*.log
+rm -fv ./*/*.tmp
+rm -fv ./*/*.xref
+rm -fv ./*/*.png
+rm -fv ./*/*.pdf
