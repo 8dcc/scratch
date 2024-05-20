bb4fef92ff364b9914b622440f663717597ccea5
8dcc <8dcc.git@gmail.com>
2024-05-21 00:03:43 +0200

    Add stub for bst-successor-search.c

---
+++ C/Misc/bst-successor-search.c
@@ -0,0 +1,25 @@
+/*
+ * BST Successor Search.
+ * ---------------------
+ *
+ * In a Binary Search Tree (BST), an Inorder Successor of a node is defined as
+ * the node with the smallest key greater Man the key of the input node (see
+ * examples below). Given a node `inputNode' in a BST, you're asked to write a
+ * function `findInOrderSuccessor' that returns the Inorder Successor of
+ * `inputNode'. If `inputNode' has no Inorder Successor, return `null'.
+ *
+ * Explain your solution and analyze its time and space complexities.
+ *
+ * For example:
+ *
+ *                        <20>
+ *                        /  \
+ *                      <9>  <25>
+ *                      / \
+ *                    <5> <12>
+ *                        /  \
+ *                     <11>  <14>
+ *
+ * In this diagram, the Inorder Successor of 9 is 11, and the Inorder Successor
+ * of 14 is 20.
+ */
