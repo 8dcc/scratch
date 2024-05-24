2377e48d6d0633af77d5dabecc596d4c97a55012
8dcc <8dcc.git@gmail.com>
2024-05-24 22:04:43 +0200

    Improve both fizzbuzz versions

--- C/misc/fizzbuzz/fizzbuzz-matrix.c
+++ C/misc/fizzbuzz/fizzbuzz-matrix.c
@@ -1,14 +1,26 @@
-#include <stdio.h>
 
-int main() {
+#include <stdio.h>
+#include <stdlib.h>
+
+static void fizzbuzz(int iterations) {
     const char* p[2][2] = {
         /* !(%3)     (%3) */
-        { "%d\n",   "Fizz\n" },     /* !(%5) */
+        { "%d\n",   "Fizz\n" },       /* !(%5) */
         { "Buzz\n", "FizzBuzz\n" }, /* (%5) */
     };
 
-    for (int i = 0; i < 100; i++)
+    for (int i = 1; i <= iterations; i++)
         printf(p[i % 5 == 0][i % 3 == 0], i);
+}
+
+int main(int argc, char** argv) {
+    if (argc < 2) {
+        fprintf(stderr, "Usage: %s <iterations>\n", argv[0]);
+        return 1;
+    }
+
+    const int iterations = atoi(argv[1]);
+    fizzbuzz(iterations);
 
     return 0;
 }
--- C/misc/fizzbuzz/fizzbuzz.c
+++ C/misc/fizzbuzz/fizzbuzz.c
@@ -1,26 +1,28 @@
+
 #include <stdio.h>
 #include <stdlib.h>
 
-void fizzbuzz_loop(const int iterations);
-const char* int2fizzbuzz(const int n);
+static void fizzbuzz_loop(int iterations) {
+    for (int i = 1; i <= iterations; i++) {
+        if (i % 15 == 0)
+            puts("FizzBuzz");
+        else if (i % 3 == 0)
+            puts("Fizz");
+        else if (i % 5 == 0)
+            puts("Buzz");
+        else
+            printf("%d\n", i);
+    }
+}
 
 int main(int argc, char** argv) {
     if (argc < 2) {
-        printf("I need the number of rounds!\n");
+        fprintf(stderr, "Usage: %s <iterations>\n", argv[0]);
         return 1;
     }
 
-    fizzbuzz_loop(atoi(argv[1]));       // Pass arg as int to func
+    const int iterations = atoi(argv[1]);
+    fizzbuzz_loop(iterations);
+
     return 0;
-}
-
-void fizzbuzz_loop(const int iterations) {
-    for (int n = 1; n < iterations; n++) {
-        if (n % 15 == 0)        printf("FizzBuzz");
-        else if (n % 3 == 0)    printf("Fizz");
-        else if (n % 5 == 0)    printf("Buzz");
-        else                    printf("%d", n);
-
-        printf("\n");
-    }
 }
