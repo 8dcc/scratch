e23e64a7f36019b2ab71c54afda086a7c3f39e10
8dcc <8dcc.git@gmail.com>
2024-06-03 00:48:43 +0200

    Clean comments in buffered-search.c

--- C/misc/buffered-search.c
+++ C/misc/buffered-search.c
@@ -1,32 +1,54 @@
+/*
+ * Search for the byte sequence `pat' from the address `start' to the address
+ * `end', while reading the memory in chunks of size `strlen(pat)'.
+ *
+ * The `end' address is NOT read. The pattern is a null-terminated byte array,
+ * and it's assumed to be greater than the search region (end minus start).
+ *
+ * The general process of the buffered search is the following:
+ *
+ * 1. Iterate while we haven't reached the `end' address, and while the pattern
+ *    isn't fully matched.
+ * 2. Check if we have reached the end of our buffer. If we have, we need to
+ *    repopulate it with new data.
+ * 2.1. If we are not inside a match, just increase the address that our buffer
+ *      is mapped to and reset the buffer and pattern positions. If we were in a
+ *      match, we want to repopulate where the match started, and we want to
+ *      continue from that position.
+ * 2.2. In either case, we want to reset the match start position inside the
+ *      buffer to zero.
+ * 2.3. We need to check if the size we are trying to read actually reads past
+ *      the `end' address. If so, only read up to that point.
+ * 2.4. Actually read the data using some kind of memcpy() function.
+ * 3. Compare the byte at the current position inside our buffer with the
+ *    current pattern character.
+ * 3.1. If it matches, just move to the next byte in the buffer and the pattern,
+ *      without modifying where the match started.
+ * 3.2. If it didn't match, increase the position where the potential match will
+ *      start, moving it to the next position in the buffer. Also reset the
+ *      position inside the pattern.
+ *
+ * Once we are done, we can know if we matched the pattern or not by checking
+ * how we exited the loop. If we read the whole pattern, we found a match.
+ */
 
 #include <stdint.h>
 #include <stdio.h>
 #include <string.h> /* strlen() */
 #include <stdlib.h> /* malloc(), free() */
 
+/* Sample data */
 static const uint8_t mem[] = "ABCDEFGHIJK";
 
-/* Just to illustrate this example. Could be replaced with `process_vm_readv'.
- *
- * NOTE: Try printing the read bytes inside this function if you have trouble
- * understanding how the buffering works. */
 void* mem_read(void* dst, const void* src, size_t sz) {
-    uint8_t* out      = dst;
-    const uint8_t* in = src;
-
-    while (sz-- > 0)
-        *out++ = *in++;
-
-    return out;
+    /* Just to illustrate this example. Could be replaced with
+     * `process_vm_readv'.
+     *
+     * NOTE: Try printing the read bytes inside this function if you have
+     * trouble understanding how the buffering works. */
+    return memcpy(dst, src, sz);
 }
 
-/* Search for the byte sequence `pat' from the address `start' to the address
- * `end', while reading the memory in chunks of size `strlen(pat)'.
- *
- * The `end' address is NOT read.
- *
- * The pattern is a null-terminated byte array, and it's assumed to be greater
- * than the search region (end-start). */
 void* buffered_search(uintptr_t start, uintptr_t end, const char* pat) {
     /* The input will be buffered, and the pattern will be searched in here.
      *
@@ -50,62 +72,35 @@ void* buffered_search(uintptr_t start, uintptr_t end, const char* pat) {
     /* Position inside the buffer where the current match started */
     int match_start = 0;
 
-    /* While we have more memory and pattern left to read */
     while ((chunk_start + buf_pos) < end && pat[pat_pos] != '\0') {
-        /* Check if we reached the end of the buffer. If so, repopulate it. */
         if (buf_pos >= buf_sz) {
             if (match_start == buf_pos) {
-                /* If we are not inside a match (because we are already at the
-                 * start of the match):
-                 *
-                 * Increase the address that we are mapping (move to the next
-                 * chunk of size `buf_sz'). */
                 chunk_start += buf_sz;
-
-                /* And reset the position inside the buffer and the pattern to
-                 * zero. */
                 buf_pos = 0;
                 pat_pos = 0;
             } else {
-                /* Otherwise, if we are not inside a match:
-                 *
-                 * Repopulate from the start of the match, and don't change the
-                 * position inside the pattern. */
                 chunk_start += match_start;
-
-                /* We matched N characters of the input, and the buffer is now
-                 * mapped to the match start. The current position in the new
-                 * buffer is the position we were on the (partial) match. */
                 buf_pos = pat_pos;
             }
 
-            /* The match start is always going to be zero on repopulation, even
-             * if we were in a match already. */
             match_start = 0;
 
-            /* Make sure we aren't reading past the `end' address */
             if (chunk_start + buf_sz > end)
                 buf_sz = end - chunk_start;
 
-            /* Repopulate the buffer with the new chunk */
             mem_read(buf, (void*)chunk_start, buf_sz);
         }
 
         if (buf[buf_pos] == pat[pat_pos]) {
-            /* We found an exact byte match with the pattern. Go to next byte in
-             * the buffer. */
             buf_pos++;
             pat_pos++;
         } else {
-            /* A byte didn't match, increase the match start position and search
-             * again from there. */
             match_start++;
             buf_pos = match_start;
             pat_pos = 0;
         }
     }
 
-    /* If we reached end of the pattern, return the match. Otherwise, NULL. */
     void* ret =
       (pat[pat_pos] == '\0') ? (void*)(chunk_start + match_start) : NULL;
 
