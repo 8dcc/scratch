4463af2ac51b01ff480b7ae99361bceaa1c2d2b1
8dcc <8dcc.git@gmail.com>
2024-06-04 17:01:55 +0200

    Improve array-index-math.c

--- C/misc/array-index-math.c
+++ C/misc/array-index-math.c
@@ -1,40 +1,34 @@
 
 #include <stdio.h>
 
-#define R 4    // Rows
-#define C 6    // Cols
+#define H 4
+#define W 6
 
-// Colors
-#define C_B "\x1B[1;37m"
-#define C_N "\x1B[0m"
+static void print_arr(int* arr) {
+    for (int y = 0; y < H; y++) {
+        for (int x = 0; x < W; x++)
+            printf("%d:%d:%02d ", y, x, arr[W * y + x]);
 
-static void print_arr(int* arr);
+        putchar('\n');
+    }
+}
 
-int main() {
-    int arr[R][C] = {
+int main(void) {
+    int arr[H][W] = {
         { 0, 1, 2, 3, 4, 5 },
         { 6, 7, 8, 9, 10, 11 },
         { 12, 13, 14, 15, 16, 17 },
         { 18, 19, 20, 21, 22, 23 },
     };
-    int* arr_p = &arr[0][0];
 
     printf("Original:\n"
            "{  0,  1,  2,  3,  4,  5 },\n"
            "{  6,  7,  8,  9, 10, 11 },\n"
            "{ 12, 13, 14, 15, 16, 17 },\n"
-           "{ 18, 19, 20, 21, 22, 23 },\n"
-           "\n");
+           "{ 18, 19, 20, 21, 22, 23 },\n\n");
+
+    int* arr_p = &arr[0][0];
     print_arr(arr_p);
 
     return 0;
-}
-
-void print_arr(int* arr) {
-    for (int y = 0; y < R; y++) {
-        for (int x = 0; x < C; x++)
-            printf("%d:%d:" C_B "%2d " C_N, y, x, arr[C * y + x]);
-
-        putchar('\n');
-    }
 }
