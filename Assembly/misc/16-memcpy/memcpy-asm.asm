0b4194a3625aacaa7fbb5b886ed2474048ea9877
8dcc <8dcc.git@gmail.com>
2024-06-04 17:21:47 +0200

    Remove unnecesary comments from memcpy.asm
    Add note about memcpy-asm.asm to Makefile

--- Assembly/misc/16-memcpy/memcpy-asm.asm
+++ Assembly/misc/16-memcpy/memcpy-asm.asm
@@ -1,11 +1,5 @@
 
-
 section .text
-
-; rax -> 8-byte (qword)
-; eax -> 4-byte (dword)
-;  ax -> 2-byte (word)
-;  al -> 1-byte (byte)
 
 ; rax memcpy_asm(void* rdi, const void* rsi, size_t rdx);
 global memcpy_asm
