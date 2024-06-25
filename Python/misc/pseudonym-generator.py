877977ff8e3f0907bd2624b201c9bb16331877f2
8dcc <8dcc.git@gmail.com>
2024-06-26 00:38:13 +0200

    Add support for multiple words in pseudonym generator

--- Python/misc/pseudonym-generator.py
+++ Python/misc/pseudonym-generator.py
@@ -17,7 +17,7 @@ def main():
         print("Usage: %s <str> <dict-file>" % sys.argv[0])
         exit(1)
 
-    input_str = sys.argv[1].strip().replace(" ", "")
+    input_str = sys.argv[1].strip()
     dict_path = sys.argv[2]
 
     # Make sure the dictionary exists
@@ -27,10 +27,35 @@ def main():
 
     # Iterate each line of the dictionary
     with open(dict_path) as f:
-        for line in f:
-            line = line.strip()
-            if valid_pseudonym(input_str.lower(), line.lower()):
-                print(line)
+        words = [line.strip() for line in f]
+
+    # Initialize the indexes array. For more information, see:
+    # https://github.com/8dcc/scratch/blob/main/C/algorithms/variable-nested-loops.c
+    input_word_count = len(input_str.split())
+    indexes = []
+    for i in range(input_word_count):
+        indexes.append(0)
+
+    while indexes[0] < len(words):
+        # Join all words in the iteration into a string
+        total_string = ""
+        for i in range(input_word_count):
+            total_string += words[indexes[i]] + " "
+        total_string = total_string.rstrip()
+
+        # Check if the word combination is a possible pseudonym
+        if valid_pseudonym(input_str.replace(" ", "").lower(),
+                           total_string.replace(" ", "").lower()):
+            print(total_string)
+
+        for cur_idx in reversed(range(input_word_count)):
+            if cur_idx == 0 or indexes[cur_idx] + 1 < len(words):
+                indexes[cur_idx] += 1
+                break
+
+            indexes[cur_idx] = 0
+
+
 
 if __name__ == '__main__':
     main()
