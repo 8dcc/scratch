adf1d929c7bec5c8fa8e5719c3575e89163a5ca3
8dcc <8dcc.git@gmail.com>
2024-07-02 23:56:18 +0200

    Improve stack-allocation.asm

--- Assembly/misc/03-stack-allocation/stack-allocation.asm
+++ Assembly/misc/03-stack-allocation/stack-allocation.asm
@@ -1,57 +1,62 @@
-; =====================================================================
-; Warning, the comments on this asm file might be too awesome for you
-; =====================================================================
 
 section .text
     global _start
 
 _start:
-    mov     ebx, 123            ; Just a test value that will be stored in the stack
+    mov     ebx, 123
     push    ebx
 
-    call    myfunc              ; Call my function
+    ; Call my function that should not modify the stack frame
+    call    myfunc
 
-    pop     ebx                 ; Pop value from stack (should be 123 if the stack was unaltered)
-    
-    mov     eax, 1              ; Exit with ebx as return value
+    ; Pop value from stack (should be 123 if the stack was unaltered)
+    pop     ebx
+
+    ; Exit with `ebx' as return value
+    mov     eax, 1
     int     0x80
 
-; You can declare functions after the call
 myfunc:
-    ; Right now the next instruction from where we called is stored in the stack,
-    ; so we could do sometihng like
-    ;   pop eax
-    ;   jmp eax
-    ; Which is exactly what `ret` does
+    ; When entering the function, the return address of the caller is on the top
+    ; of the stack. The `ret' instruction would "pop it" and jump to it.
+    ;
+    ; After entering, we push `ebp' for preserving the initial value of `esp'.
+    ;   +---+---+---+---+---+---+---+---+---+---+
+    ;   | ? |   |   |   |rbp|ret|123|...|...|...|
+    ;   +---+---+---+---+---+---+---+---+---+---+
+    ;                             ^ Value pushed in _start
+    ;                     ^ ebp/esp
+    push    ebp
+    mov     ebp, esp
 
-    mov     ebp, esp            ; We use the ebp register to store the current position of the stack
-                                ; so we can restore it later into esp and have an unaltered stack
+    ; Because the top of the stack is in a lower memory address, we subtract 3
+    ; from `esp' so we move that pointer to a previous position of the stack,
+    ; allocating 3 bytes.
+    ;   +---+---+---+---+---+---+---+---+---+---+
+    ;   | ? |   |   |   |rbp|ret|123|...|...|...|
+    ;   +---+---+---+---+---+---+---+---+---+---+
+    ;         ^ esp after instruction
+    sub     esp, 3
 
-    sub     esp, 3              ; Because the top of the stack is in a "lower" position than the
-                                ; bottom parts (pushes to a previous address in memory) we subtract
-                                ; 3 from esp so we move that pointer to a previous position of the
-                                ; stack, allocating 3 bytes. Current stack representation:
-                                ; +---+---+---+---+---+---+---+---+---+
-                                ; | ? |   |   |   |ret|123|...|...|...|
-                                ; +---+---+---+---+---+---+---+---+---+
-                                ;       ^           ^   ^ Sould be ebx from _start
-                                ;       ^           ^ Pointer to next instruction after call (ebp now)
-                                ;       ^ Stack pointer after instruction
+    ; We write the 3 bytes. We don't need a null-terminator in this case since
+    ; we know we are going to be writing exactly 3 bytes.
+    ;   +---+---+---+---+---+---+---+---+---+---+
+    ;   | ? |':'|')'|0xA|rbp|ret|123|...|...|...|
+    ;   +---+---+---+---+---+---+---+---+---+---+
+    mov     [esp],   byte ':'
+    mov     [esp+1], byte ')'
+    mov     [esp+2], byte 0xA
 
-    mov     [esp],   byte ':'   ; +-----+-----+-----+-----+-----+-----+-----+
-    mov     [esp+1], byte '3'   ; |  ?  | ':' | '3' |'\n' | ret | 123 | ... |
-    mov     [esp+2], byte 0xA   ; +-----+-----+-----+-----+-----+-----+-----+
-                                ;          ^ esp             ^ ebp
-                                ; Maybe it would be a good idea to add a '\0', but we will use `edx, 3`
-
-    mov     eax, 4
-    mov     ebx, 1
-    mov     ecx, esp            ; Because esp is pointing to ':' we can just move that address to ecx
+    ; Since esp is pointing to the string we can use it directly when calling
+    ; SYS_WRITE.
     mov     edx, 3
-    int     0x80                ; Print
+    mov     ecx, esp
+    mov     ebx, 1              ; STDOUT
+    mov     eax, 4              ; SYS_WRITE
+    int     0x80
 
-    ; Now keep in mind that esp is still pointing to ':' and we need esp to be the return address
-    ; (next instruction after call, in this case `pop ebx`). That's why we stored esp into ebp
-    mov     esp, ebp            ; Restore stack pointer
-    ret                         ; We can safely return now
-
+    ; We could simply add 3 to `esp', but since we preserved the stack frame, we
+    ; can just restore `ebp' and return.
+    mov     esp, ebp
+    pop     ebp
+    ret
