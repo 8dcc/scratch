1fb23f2ccfe470fa0a8659d907f8cbccfdfc65c6
8dcc <8dcc.git@gmail.com>
2024-07-02 23:37:30 +0200

    Remove unnecesary comments from modify-string.asm

--- Assembly/misc/02-modify-string/modify-string.asm
+++ Assembly/misc/02-modify-string/modify-string.asm
@@ -1,31 +1,24 @@
+
 section .data
-    msg1        db  "Hello world!", 0xA, 0x0    ; str + '\n' + '\0'
-    msg1_len    equ $-msg1                      ; cur pointer pos - msg1 pointer pos
+    msg1        db  "Hello world!", 0xA
+    msg1_len    equ $-msg1
 
 section .text
     global _start
 
-print_msg:                          ; Create label that will be called as func to print msg1
-    mov     eax, 4                  ; sys_write
-    mov     ebx, 1                  ; stdout
-    mov     ecx, msg1               ; msg1 pointer
-    mov     edx, msg1_len           ; Lenght stored in variable
-    int     0x80                    ; Kernel interrupt
-    ret                             ; Return function
-
-exit:                               ; Didn't want to add the "library" from the other folders
-    mov     eax, 1
-    mov     ebx, 0
+print_msg:
+    mov     edx, msg1_len
+    mov     ecx, msg1
+    mov     ebx, 1                  ; STDOUT
+    mov     eax, 4                  ; SYS_WRITE
     int     0x80
-    ret                             ; Should never reach here anyway
+    ret
 
 _start:
-    call    print_msg               ; Call function to print the orignal message
+    call    print_msg
+    mov     [msg1+5], byte '_'
+    call    print_msg
 
-    mov     [msg1+7], byte '0'      ; Add 7 to msg1 pointer to access the letter 'o', then move the new char with
-                                    ; byte type (same as C char). Also the char can be inside '' but it just acts
-                                    ; like in C, using its numerical value.
-    call    print_msg               ; Call function again to print changes
-
-    call    exit                    ; Exit with exit code 0
-
+    mov     ebx, 0                  ; Exit code
+    mov     eax, 1                  ; SYS_EXIT
+    int     0x80
