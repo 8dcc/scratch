b26ba414758fe112751407b35c4530683c455980
8dcc <8dcc.git@gmail.com>
2024-11-02 01:51:21 +0100

    Add Error and Is_Empty to buffer_system

--- Ada/johnbarnes/3.1-buffer-system/buffer_system.adb
+++ Ada/johnbarnes/3.1-buffer-system/buffer_system.adb
@@ -2,6 +2,12 @@
 package body Buffer_System is
     procedure Load(B : out Buffer; S : in String) is
     begin
+        -- Raise an exception if the input string doesn't fit in the buffer, or
+        -- if the buffer has unread data.
+        if S'Length > Max or B.Start <= B.Finish then
+            raise Error;
+        end if;
+
         B.Start := 1;
         B.Finish := S'Length;
         B.Data(B.Start .. B.Finish) := S;
@@ -9,7 +15,18 @@ package body Buffer_System is
 
     procedure Get(B : in out Buffer; C : out Character) is
     begin
+        if Is_Empty(B) then
+            raise Error;
+        end if;
+
         C := B.Data(B.Start);
         B.Start := B.Start + 1;
     end Get;
+
+    function Is_Empty(B : in Buffer) return Boolean is
+    begin
+        -- I interpret an empty buffer as a buffer that doesn't have any data
+        -- that can be returned by `Get'.
+        return B.Start > B.Finish;
+    end Is_Empty;
 end Buffer_System;
--- Ada/johnbarnes/3.1-buffer-system/buffer_system.ads
+++ Ada/johnbarnes/3.1-buffer-system/buffer_system.ads
@@ -1,8 +1,18 @@
 
 package Buffer_System is
+    -- If this exception is raised, the Ada Run Time System will print the
+    -- following error message:
+    --
+    --   raised BUFFER_SYSTEM.ERROR : buffer_system.adb:123
+    --
+    -- Each exception can be identified by its context, there is no need to
+    -- define any error code.
+    Error: exception;
+
     type Buffer is private;
     procedure Load(B : out Buffer; S : in String);
     procedure Get(B : in out Buffer; C : out Character);
+    function Is_Empty(B : in Buffer) return Boolean;
 private
     Max: constant Integer := 80;
     type Buffer is
