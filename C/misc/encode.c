0e32313f790fcecabc4f56b681a23fb89c63e982
8dcc <8dcc.git@gmail.com>
2025-01-17 21:44:47 +0100

    Update 'encode.c', make unix-like

--- C/misc/encode.c
+++ C/misc/encode.c
@@ -1,34 +1,37 @@
+/*
+ * Copyright 2025 8dcc
+ *
+ * This program is free software: you can redistribute it and/or modify it under
+ * the terms of the GNU General Public License as published by the Free Software
+ * Foundation, either version 3 of the License, or any later version.
+ *
+ * This program is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU General Public License for more
+ * details.
+ *
+ * You should have received a copy of the GNU General Public License along with
+ * this program. If not, see <https://www.gnu.org/licenses/>.
+ */
 
+#include <limits.h>
 #include <stdio.h>
 
-#define OFFSET 100
-
-int main() {
-    char filename[100];
-
-    printf("Filename: ");
-    scanf("%100s", filename);
-
-    FILE* original_file = fopen(filename, "r");
-    if (!original_file) {
-        printf("Error. File does not exist.\n");
+int main(int argc, char** argv) {
+    if (argc != 2) {
+        fprintf(stderr, "Usage: %s OFFSET < INPUT.txt\n", argv[0]);
         return 1;
     }
 
-    FILE* output_file = fopen("encoded.txt", "w");
-    if (!output_file)
+    int offset;
+    if (sscanf(argv[1], "%d", &offset) != 1) {
+        fprintf(stderr, "Error: Invalid offset format.\n");
         return 1;
-
-    char c;
-    while ((c = fgetc(original_file)) != EOF) {
-        c += OFFSET;
-        fputc(c, output_file);
     }
 
-    fclose(original_file);
-    fclose(output_file);
-
-    printf("Done. Check encoded.txt\n");
+    int c;
+    while ((c = getchar()) != EOF)
+        putchar((c + offset) % CHAR_MAX);
 
     return 0;
 }
