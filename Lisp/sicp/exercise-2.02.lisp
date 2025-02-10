21d480665e0054d8f60ccb2a6e333acc13e1e6d8
8dcc <8dcc.git@gmail.com>
2025-02-10 22:27:43 +0100

    Add SICP exercise 2.2

---
+++ Lisp/sicp/exercise-2.02.lisp
@@ -0,0 +1,66 @@
+
+;; Exercise 2.2
+;; ============
+;;
+;; Consider the problem of representing line segments in a plane. Each segment
+;; is represented as a pair of points: a starting point and an ending
+;; point. Define a constructor `make-segment' and selectors `start-segment' and
+;; `end-segment' that define the representation of segments in terms of
+;; points. Furthermore, a point can be represented as a pair of numbers: the X
+;; coordinate and the Y coordinate. Accordingly, specify a constructor
+;; `make-point' and selectors `x-point' and `y-point' that define this
+;; representation. Finally, using your selectors and constructors, define a
+;; procedure `midpoint-segment' that takes a line segment as argument and
+;; returns its midpoint (the point whose coordinates are the average of the
+;; coordinates of the endpoints).
+
+;; -----------------------------------------------------------------------------
+;; Dependencies
+
+(defun as-unified-num (n)
+  (cond ((flt? n) n)
+        ((int? n) (int->flt n))
+        (tru (error "Invalid type."))))
+
+(defun average (a b)
+  (/ (+ a b) 2))
+
+;; -----------------------------------------------------------------------------
+;; Exercise
+
+(defun make-point (x y)
+  (cons x y))
+(defun x-point (p)
+  (as-unified-num (car p)))
+(defun y-point (p)
+  (as-unified-num (cdr p)))
+
+(defun make-segment (start end)
+  (cons start end))
+(defun start-segment (s)
+  (car s))
+(defun end-segment (s)
+  (cdr s))
+
+(defun midpoint-segment (s)
+  (let ((start (start-segment s))
+        (end   (end-segment s)))
+    (make-point (average (x-point start) (x-point end))
+                (average (y-point start) (y-point end)))))
+
+;; -----------------------------------------------------------------------------
+;; Examples
+
+(defun format-point (p)
+  (format "(%f,%f)" (x-point p) (y-point p)))
+
+(defun format-segment (s)
+  (format "%s-%s"
+          (format-point (start-segment s))
+          (format-point (end-segment s))))
+
+(let ((segment (make-segment (make-point 2 2)
+                             (make-point 4 5))))
+  (format "%s => %s"
+          (format-segment segment)
+          (format-point (midpoint-segment segment))))
