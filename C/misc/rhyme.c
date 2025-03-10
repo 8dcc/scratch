06153e6c6e323be46074cf9f8eb116938e58db71
8dcc <8dcc.git@gmail.com>
2025-03-10 11:21:08 +0100

    Store match information in 'rhyme.c'

--- C/misc/rhyme.c
+++ C/misc/rhyme.c
@@ -27,10 +27,17 @@
     } while (0)
 
 /*----------------------------------------------------------------------------*/
+/* Structures */
 
 typedef struct Word {
     char* str;
     size_t len;
+
+    /*
+     * Ratio [0..1] between the number of matching characters with a target
+     * word, and the length of the word.
+     */
+    float match;
 } Word;
 
 typedef struct Dict {
@@ -39,6 +46,7 @@ typedef struct Dict {
 } Dict;
 
 /*----------------------------------------------------------------------------*/
+/* Misc helpers */
 
 void* safe_malloc(size_t size) {
     void* result = malloc(size);
@@ -60,9 +68,10 @@ void safe_realloc(void* double_ptr, size_t size) {
 }
 
 /*----------------------------------------------------------------------------*/
+/* Dictionary initialization and deinitialization */
 
 /*
- * Initialize and return a dictionary containing each non-empty line of thje
+ * Initialize and return a dictionary containing each non-empty line of the
  * specified file.
  */
 static Dict dict_read(FILE* fp) {
@@ -95,7 +104,8 @@ static Dict dict_read(FILE* fp) {
              * a new word buffer).
              */
             assert(cur_word_pos <= cur_word_sz);
-            result.words[dict_pos].len = cur_word_pos;
+            result.words[dict_pos].len   = cur_word_pos;
+            result.words[dict_pos].match = 0.f;
             cur_word_pos = cur_word_sz = 0;
             dict_pos++;
             continue;
@@ -155,6 +165,53 @@ static void dict_free(Dict dict) {
     free(dict.words);
 }
 
+/*----------------------------------------------------------------------------*/
+/* Dictionary matching */
+
+/*
+ * Return the number of matching characters starting from the end of A and B.
+ */
+static inline int rightmost_matches(const char* a, size_t a_sz, const char* b,
+                                    size_t b_sz) {
+    const char* a_right = &a[a_sz - 1];
+    const char* b_right = &b[b_sz - 1];
+
+    int result = 0;
+    while (a <= a_right && b <= b_right && *a_right == *b_right) {
+        result++;
+        a_right--;
+        b_right--;
+    }
+
+    return result;
+}
+
+/*
+ * Store the "match ratio" of each word in the dictionary when compared to a
+ * target.
+ *
+ * The "match ratio" of a word and a target is calculated by dividing the number
+ * of right-most matching characters between the two strings (according to the
+ * 'rightmost_matches' function) by the length of the target.
+ *
+ * For example, a word "aaabbb" and a target "bb" would have a 1.0 ratio, since
+ * all characters of the target match the right-most characters of the word.
+ */
+static void dict_match(Dict dict, const char* target) {
+    const size_t target_len = strlen(target);
+
+    for (size_t i = 0; i < dict.size; i++) {
+        const char* cur_str  = dict.words[i].str;
+        const size_t cur_len = dict.words[i].len;
+        const int match_num =
+          rightmost_matches(target, target_len, cur_str, cur_len);
+        dict.words[i].match = (float)match_num / target_len;
+    }
+}
+
+/*----------------------------------------------------------------------------*/
+/* Dictionary printing */
+
 /*
  * Print all possible information about a dictionary.
  */
@@ -163,28 +220,30 @@ static inline void dict_print(Dict dict) {
         printf("[%zu] ", i);
         for (size_t j = 0; j < dict.words[i].len; j++)
             putchar(dict.words[i].str[j]);
-        putchar('\n');
+        printf(" (%.2f)\n", dict.words[i].match);
     }
 }
 
+/*----------------------------------------------------------------------------*/
+/* Entry point */
+
 int main(int argc, char** argv) {
-    if (argc != 2) {
-        ERR("Usage: %s DICT-FILE", argv[0]);
+    if (argc != 3) {
+        ERR("Usage: %s DICT_FILE TARGET_WORD", argv[0]);
         return 1;
     }
 
     const char* dict_pathname = argv[1];
-    FILE* dict_fp             = fopen(dict_pathname, "r");
+    const char* target_word   = argv[2];
+
+    FILE* dict_fp = fopen(dict_pathname, "r");
     if (dict_fp == NULL) {
         ERR("Could not open '%s': %s", dict_pathname, strerror(errno));
         return 1;
     }
 
-    /*
-     * NOTE: The current program doesn't operate on the dictionary, it just
-     * prints it.
-     */
     Dict dict = dict_read(dict_fp);
+    dict_match(dict, target_word);
     dict_print(dict);
 
     dict_free(dict);
