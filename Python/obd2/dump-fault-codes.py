ab42db4440a713d36053b0f83e1b8ef6341d65cb
8dcc <8dcc.git@gmail.com>
2025-09-27 20:42:39 +0200

    python: Remove unnecessary includes from 'dump-fault-codes.py'

--- Python/obd2/dump-fault-codes.py
+++ Python/obd2/dump-fault-codes.py
@@ -15,10 +15,8 @@
 # You should have received a copy of the GNU General Public License along with
 # this program.  If not, see <https://www.gnu.org/licenses/>.
 
-import obd
-import time
 import sys
-from datetime import datetime
+import obd
 
 def err(msg):
     sys.stderr.write(msg + "\n")
