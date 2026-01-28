1dd922806206103ed6918a5480ca4b7e6b53881c
8dcc <8dcc.git@gmail.com>
2026-01-28 19:33:08 +0100

    wavelength-calculator: Add 'wavelength2freq'
    Add more frequencies, print 1/32 of wavelength.

--- C/misc/wavelength-calculator.c
+++ C/misc/wavelength-calculator.c
@@ -21,10 +21,17 @@
 #define LIGHT_SPEED 299792458 /* meters per second */
 
 /*
- * Return the wavelength of the specified frequency (in Hertz).
+ * Return the wavelength (in meters) of the specified frequency (in Hertz).
  */
 static inline double freq2wavelength(double freq) {
     return LIGHT_SPEED / freq;
+}
+
+/*
+ * Return frequency (in Hertz) of the specified the wavelength (in meters).
+ */
+static inline double wavelength2freq(double wavelength) {
+    return LIGHT_SPEED / wavelength;
 }
 
 /*----------------------------------------------------------------------------*/
@@ -33,27 +40,35 @@ static inline double freq2wavelength(double freq) {
 
 int main(void) {
     double freqs[] = {
-        7000000,   /* 7 MHz (40 meter band) */
-        14000000,  /* 14 MHz (20 meter band) */
-        27000000,  /* 27 MHz (CB) */
-        29000000,  /* 29 MHz (10 meter band) */
+        7000000, /* 7 MHz (40 meter band) */
+        wavelength2freq(40),
+        14000000, /* 14 MHz (20 meter band) */
+        wavelength2freq(20),
+        27000000, /* 27 MHz (CB) */
+        wavelength2freq(11),
+        29000000, /* 29 MHz (10 meter band) */
+        wavelength2freq(10),
         145000000, /* 145 MHz (Repeaters) */
+        wavelength2freq(2),
+        wavelength2freq(0.70),
         446000000, /* 446 MHz (PMR) */
     };
 
-    printf("| Freq (MHz) | Wavelength (m) | 1/2     | 1/4     | 1/8     | 1/16 "
-           "   |\n"
-           "|------------+----------------+---------+---------+---------+------"
-           "---|\n");
+    /* clang-format off */
+    printf("| Freq (MHz) | Wavelength (m) | 1/2     | 1/4     | 1/8     | 1/16    | 1/32    |\n"
+           "|------------+----------------+---------+---------+---------+---------+---------|\n");
+    /* clang-format on */
+
     for (size_t i = 0; i < LENGTH(freqs); i++) {
         const double wavelength = freq2wavelength(freqs[i]);
-        printf("| %10.2f | %14.4f | %7.4f | %7.4f | %7.4f | %7.4f |\n",
+        printf("| %10.2f | %14.4f | %7.4f | %7.4f | %7.4f | %7.4f | %7.4f |\n",
                freqs[i] / 1000000,
                wavelength,
                wavelength / 2,
                wavelength / 4,
                wavelength / 8,
-               wavelength / 16);
+               wavelength / 16,
+               wavelength / 32);
     }
 
     return 0;
